c6084dc3b867dd497399d3098e63f359a4c3346c
Lxk_buaa <867768510@qq.com>
2026-09-17 01:22:41 +0800

    test: keep jest-environment pragma ahead of license headers

--- packages/suite-base/src/panels/ThreeDeeRender/Renderer.batch.test.ts
+++ packages/suite-base/src/panels/ThreeDeeRender/Renderer.batch.test.ts
@@ -1,7 +1,7 @@
+/** @jest-environment jsdom */
+
 // SPDX-FileCopyrightText: Copyright (C) 2023-2026 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)<lichtblick@bmwgroup.com>
 // SPDX-License-Identifier: MPL-2.0
-
-/** @jest-environment jsdom */
 
 // SPDX-FileCopyrightText: Copyright (C) 2026 XGC-Team
 // SPDX-License-Identifier: MPL-2.0
